7dd2a4568ae2a893f6df41ceaede88fdfb37813d
Ilie <ischiopu48@boscdelacoma.cat>
2024-11-18 19:16:22 +0100

    added funtionality to pass the initialization test

--- src/app/features/sda-vectors/sda-vectors.component.ts
+++ src/app/features/sda-vectors/sda-vectors.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sda-vectors',
-  standalone: true,
   imports: [],
   templateUrl: './sda-vectors.component.html',
   styleUrl: './sda-vectors.component.css'
 })
 export class SdaVectorsComponent {
-
+  vectors = [
+    { nom: 'Aprenentatges competencials', marca: false },
+    { nom: 'Perspectiva de gènere', marca: false },
+    { nom: "Qualitat de l'educació de les llengües", marca: false },
+    { nom: 'Benestar emocional', marca: false },
+    { nom: 'Universalitat del currículum', marca: false },
+    { nom: 'Ciutadania democràtica i consciència global', marca: false },
+  ];
 }
