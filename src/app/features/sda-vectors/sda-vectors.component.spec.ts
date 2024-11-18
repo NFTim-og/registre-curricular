8b5462313a4abd9a26948acef19db438e8fbfc76
Ilie <ischiopu48@boscdelacoma.cat>
2024-11-18 18:53:08 +0100

    added first test must have 6 vectors initialized as not marked

--- src/app/features/sda-vectors/sda-vectors.component.spec.ts
+++ src/app/features/sda-vectors/sda-vectors.component.spec.ts
@@ -1,23 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SdaVectorsComponent } from './sda-vectors.component';
 
-describe('SdaVectorsComponent', () => {
+describe('VectorsChecklistComponent', () => {
   let component: SdaVectorsComponent;
   let fixture: ComponentFixture<SdaVectorsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SdaVectorsComponent]
-    })
-    .compileComponents();
+      declarations: [SdaVectorsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SdaVectorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('It must have 6 vectors initialized as not marked', () => {
+    expect(component.vectors.length).toBe(6);
+    expect(component.vectors.every((vector) => !vector.marca)).toBeTrue();
   });
 });
+
