c99dacbd0015843b713dc6d287cc5d6107ad6e8d
Guanlin Wang <guanlin987@gmail.com>
2024-11-19 16:12:55 +0100

    updated the html for it to have a toggeble list

--- src/app/features/side-navigation-bar/side-navigation-bar.component.ts
+++ src/app/features/side-navigation-bar/side-navigation-bar.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './side-navigation-bar.component.html',
   styleUrl: './side-navigation-bar.component.css'
 })
