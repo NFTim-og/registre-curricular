13842452d4122559b70a6f5928d6c17d7d77e671
Pierre <pierredanjonrgmail.com>
2024-11-19 16:09:19 +0100

    prova bar progression html percertatge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SdaBasicInfoComponent } from "./features/sda-basic-info/sda-basic-info.component";
+import { SdACompetencesComponent } from './sda-competences/sda-competences.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SdaBasicInfoComponent],
+  imports: [RouterOutlet, SdaBasicInfoComponent, SdACompetencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
