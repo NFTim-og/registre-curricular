b4796493b905ebcaaed61f35359b8d0973da2de5
Pierre <pierredanjonrgmail.com>
2024-11-20 16:42:52 +0100

    recuperació del component sda-competences + creació sda-sabers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SdaBasicInfoComponent } from "./features/sda-basic-info/sda-basic-info.component";
-import { SdACompetencesComponent } from './sda-competences/sda-competences.component';
+import { SdACompetencesComponent } from './features/sda-competences/sda-competences.component';
 import { SideNavigationBarComponent } from './features/side-navigation-bar/side-navigation-bar.component';
 
 @Component({
