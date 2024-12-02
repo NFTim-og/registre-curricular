cde8ee3cbe961c88c4fee50c749324ebeb044680
Pierre <pierredanjonrgmail.com>
2024-12-02 19:45:37 +0100

    test 2 should display the user and password in the template

--- src/app/features/sda-login/sda-login.component.spec.ts
+++ src/app/features/sda-login/sda-login.component.spec.ts
@@ -25,11 +25,33 @@ describe('SdALoginComponent', () => {
       expect(component.loginDetails).toEqual(loginData);
     });
 
-    it('should contain `usuari` and `contrasenya` in the login data', () => {
-      expect(component.loginDetails.usuari).toBeDefined();
-      expect(component.loginDetails.usuari).toBe('prof@example.com');
-      expect(component.loginDetails.contrasenya).toBeDefined();
-      expect(component.loginDetails.contrasenya).toBe('password123');
+    it('should contain user and password in the login data', () => {
+      component.loginDetails.user = "prof@example.com";
+      component.loginDetails.password = "password123";
+      expect(component.loginDetails.user).toBeDefined();
+      expect(component.loginDetails.user).toBe('prof@example.com');
+      expect(component.loginDetails.password).toBeDefined();
+      expect(component.loginDetails.password).toBe('password123');
     });
   });
+
+  describe('Template rendering', () => {
+    it('should display the user and password in the template', () => {
+      fixture.detectChanges(); 
+  
+      const compiled = fixture.nativeElement;
+  
+      const userElement = compiled.querySelector('#user');
+      const passwordElement = compiled.querySelector('#password');
+  
+      expect(userElement).toBeTruthy();
+      expect(passwordElement).toBeTruthy();
+  
+      expect(userElement.textContent).toContain('User: prof@example.com');
+      expect(passwordElement.textContent).toContain('Password: password123');
+    });
+  });
+  
+  
+  
 });
