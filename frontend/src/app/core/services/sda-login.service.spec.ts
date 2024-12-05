e0fdb081489ca377a3d9e3ab868a73652e17857f
Guanlin Wang <guanlin987@gmail.com>
2024-12-05 19:37:07 +0100

    implemented the test for the service to send a login request to backend and return response

--- frontend/src/app/core/services/sda-login.service.spec.ts
+++ frontend/src/app/core/services/sda-login.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { SdaLoginService } from './sda-login.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-describe('SdaLoginService', () => {
+describe('SdALoginService', () => {
   let service: SdaLoginService;
+  let httpMock: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        SdaLoginService,
+        provideHttpClientTesting(),  
+      ],
+    });
     service = TestBed.inject(SdaLoginService);
+    httpMock = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should send login request to backend and return response', () => {
+    const mockResponse = { success: true, message: 'Login successful' };
+    const user = 'test@example.com';
+    const password = 'test123';
+
+    service.login(user, password).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/api/login');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ user, password });
+    req.flush(mockResponse);
   });
 });
