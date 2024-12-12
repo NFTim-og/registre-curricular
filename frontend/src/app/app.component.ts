a456d72ca2110506b567a5663889f32b98970076
Tim <timgomperts@gmail.com>
2024-12-12 18:42:09 +0100

    Added SDA BASIC INFO to HTML

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { SideNavigationBarComponent } from './components/side-navigation-bar/side-navigation-bar.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserService } from './core/services/user/user.service';
+import { SdaBasicInfoComponent } from './components/sda/sda-basic-info/sda-basic-info.component';
 import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideNavigationBarComponent, LoginComponent, TopNavigationComponent, NgIf],
+  imports: [RouterOutlet, SdaBasicInfoComponent, SideNavigationBarComponent, LoginComponent, TopNavigationComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
