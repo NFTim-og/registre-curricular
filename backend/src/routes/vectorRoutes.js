8116acc6c237e640480e05dbd50045f5fd44d87e
Guanlin Wang <guanlin987@gmail.com>
2025-01-13 19:12:28 +0100

    updated vectorRoutes to search for the vectors by idcurs

--- backend/src/routes/vectorRoutes.js
+++ backend/src/routes/vectorRoutes.js
@@ -4,15 +4,29 @@ const { connectToDB, mssql } = require('../config/dbConfig');
 const router = express.Router();
 
 router.post('/api/v1/vector', async (req, res) => {
+  const { idCurs } = req.body; 
+
+  if (!idCurs) {
+    return res.status(400).json({ success: false, message: 'idCurs is required' });
+  }
+
   try {
     const pool = await connectToDB();
-    
+
+    const query = `
+      SELECT VP.idVectorPlantilla, VP.descripcioVector
+      FROM VectorPlantilla VP
+      INNER JOIN SdA ON SdA.idCurs = VP.idCurs
+      WHERE VP.idCurs = @idCurs
+    `;
+
     const result = await pool
       .request()
-      .query('SELECT idVectorPlantilla, descripcioVector FROM VectorPlantilla ');
-    
-    console.log('VectorPlantilla retrieved successfully');
-    console.log(result);
+      .input('idCurs', mssql.UniqueIdentifier, idCurs) 
+      .query(query);
+
+    console.log('Vectores retrieved successfully:', result.recordset);
+
     res.status(200).json({ success: true, data: result.recordset });
   } catch (err) {
     console.error('Error querying VectorPlantilla:', err);
